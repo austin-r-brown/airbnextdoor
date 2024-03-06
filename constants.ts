ae05b167d7d5ad101b778e14ba7c426bdae17f7f
austin-r-brown <austinworb@gmail.com>
2024-03-05 17:02:55 -0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -6,5 +6,5 @@ export const MS_IN_MINUTE: number = 60000;
 export const LOCALE: string = 'en';
 export const INTERVAL: number = (Number(process.env.INTERVAL_MINS) || 5) * MS_IN_MINUTE;
 export const SEND_DEBOUNCE_TIME: number = 1000;
-export const EMAIL_TIMEOUT = 10000;
+export const EMAIL_TIMEOUT: number = 10000;
 export const SUCCESS_TIMEOUT: number = Math.max(INTERVAL * 3 + EMAIL_TIMEOUT, 600000);
