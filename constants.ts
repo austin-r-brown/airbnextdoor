8dfdeb4bcda81961ce4abecc44e6e78bed5fa505
Austin Brown <austin.brown@infor.com>
2024-03-18 14:17:07 -0700

    Set minimum interval

--- constants.ts
+++ constants.ts
@@ -4,7 +4,11 @@ export const MS_IN_DAY: number = 86400000;
 export const MS_IN_MINUTE: number = 60000;
 
 export const LOCALE: string = 'en';
-export const INTERVAL: number = (Number(process.env.INTERVAL_MINS) || 5) * MS_IN_MINUTE;
-export const SEND_DEBOUNCE_TIME: number = 1000;
 export const EMAIL_TIMEOUT: number = 10000;
+export const SEND_DEBOUNCE_TIME: number = 1000;
+export const INTERVAL: number = Math.max(
+  // Default interval of 5 minutes, minimum of 10 seconds
+  (Number(process.env.INTERVAL_MINS) || 5) * MS_IN_MINUTE,
+  EMAIL_TIMEOUT
+);
 export const WATCHDOG_TIMEOUT: number = Math.max(INTERVAL * 3 + EMAIL_TIMEOUT, 600000);
