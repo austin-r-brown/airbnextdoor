d5051593c77670e9e3a70bd441d801b6675d5c79
Austin Brown <austin.brown@infor.com>
2024-04-24 11:19:32 -0700

    todo comment

--- start.ts
+++ start.ts
@@ -2,6 +2,7 @@ import { LogService } from './services/log.service';
 import { CoreService } from './services/core.service';
 
 export const startApp = () => {
+  //TODO instantiate more services here that don't need a new instance for every url
   const log = new LogService();
 
   process.env.AIRBNB_URL?.trim()
