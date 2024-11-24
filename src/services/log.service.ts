13cfb7085154e96d6d58df411a49bdfd8f63e9fb
Austin Brown <austin.brown@infor.com>
2024-11-24 08:04:05 -0800

    Remove console clearing for running as Raspberry Pi service

--- src/services/log.service.ts
+++ src/services/log.service.ts
@@ -5,9 +5,6 @@ import { BookingChange, LogItem } from '../constants/types';
 import { ConsoleType } from '../constants/enums';
 import { readFileSync } from 'fs';
 
-const START_MSG = 'Starting application';
-const SUCCESS_MSG = 'Airbnb dates checked successfully';
-
 /** Service for handling console log messages */
 export class LogService {
   private readonly logged: LogItem[] = [];
@@ -17,9 +14,8 @@ export class LogService {
   }
 
   constructor() {
-    this.resetConsole();
     const { version } = JSON.parse(readFileSync('package.json', 'utf-8'));
-    this.info(`${START_MSG} v${version}`);
+    this.info(`Starting application v${version}`);
   }
 
   private log(values: any[], type: ConsoleType) {
@@ -58,17 +54,6 @@ export class LogService {
 
   public warn(...args: any) {
     this.log(args, ConsoleType.Warn);
-  }
-
-  public success() {
-    const [lastItem] = this.logged[this.logged.length - 1];
-    if (typeof lastItem[0] === 'string' && lastItem[0].startsWith(SUCCESS_MSG)) {
-      // Only display one success message at a time
-      lastItem[0] = `${SUCCESS_MSG} ${this.timestamp}`;
-      this.resetConsole();
-    } else {
-      this.info(SUCCESS_MSG);
-    }
   }
 
   public notification(title: string, booking: Booking, change?: BookingChange) {
